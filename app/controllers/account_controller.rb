fc0ee1124f71191f9f890259e883858420baf332
David Celis <me@davidcel.is>
2013-02-17 19:19:20 -0800

    Use Strong Parameters in AccountController
    Signed-off-by: David Celis <me@davidcel.is>

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -7,13 +7,13 @@ class AccountController < ApplicationController
 
   # POST /account
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       cookies.permanent[:auth_token] = @user.auth_token
-      redirect_to root_path, :notice => 'Welcome to goodbre.ws!'
+      redirect_to root_path, notice: 'Welcome to goodbre.ws!'
     else
-      render :action => :new, :status => :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -23,15 +23,15 @@ class AccountController < ApplicationController
 
   # PUT /account
   def update
-    unless current_user.authenticate(params[:user].delete(:current_password))
+    unless current_user.authenticate(user_params[:current_password])
       current_user.errors.add(:current_password, 'was incorrect')
-      render :action => :edit and return
+      render :edit and return
     end
 
-    if current_user.update_attributes params[:user]
+    if current_user.update_attributes(user_params)
       redirect_to dashboard_path
     else
-      render :action => :edit
+      render :edit
     end
   end
 
@@ -39,6 +39,11 @@ class AccountController < ApplicationController
   def destroy
     current_user.destroy
     cookies.delete(:auth_token)
-    redirect_to welcome_path, :notice => 'Come back some day!'
+    redirect_to welcome_path, notice: 'Come back some day!'
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :name, :city, :region, :country, :password, :password_confirmation, :current_password)
+    end
 end
--- db/migrate/20130210165735_create_users.rb
+++ db/migrate/20130210165735_create_users.rb
@@ -9,15 +9,13 @@ class CreateUsers < ActiveRecord::Migration
       t.string   :password_reset_token
       t.datetime :password_reset_sent_at
 
-      t.string   :first_name
-      t.string   :last_name
-
+      t.string   :name
       t.string   :city
       t.string   :region
       t.string   :country
 
-      t.index :username, unique: true
-      t.index :email,    unique: true
+      t.index    :username, unique: true
+      t.index    :email,    unique: true
 
       t.timestamps
     end
